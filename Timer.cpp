3dd7a69eebc80b8daa926b72101de92797bd73cf
Kipjatkov Evgenij <kipiatkov.ev@phystech.edu>
2020-11-25 21:39:53 +0300

    Convert tabs to spaces in the source

--- Timer.cpp
+++ Timer.cpp
@@ -25,43 +25,43 @@ using std::setfill;
 
 Timer::Timer() {}
 void Timer::start_timer() {
-	start = steady_clock::now();
+    start = steady_clock::now();
 };
 
 void Timer::stop_timer() {
-	end = steady_clock::now();
-	ms += duration_cast<milliseconds>(end - start);
+    end = steady_clock::now();
+    ms += duration_cast<milliseconds>(end - start);
 };
 
 void Timer::clear_timer() {
-	ms = milliseconds(0);
+    ms = milliseconds(0);
 };
 
 
 void Timer::print_time() {
-	cout << ms.count() << endl;
+    cout << ms.count() << endl;
 };
 
 int Timer::get_elapsed_time(char fmt) {
-	switch (fmt) {
-	case 's':
-		return duration_cast<seconds>(ms).count();
-	case 'm':
-		return duration_cast<minutes>(ms).count();
-	case 'h':
-		return duration_cast<hours>(ms).count();
-	default:
-		return -1;
-	}
+    switch (fmt) {
+    case 's':
+        return duration_cast<seconds>(ms).count();
+    case 'm':
+        return duration_cast<minutes>(ms).count();
+    case 'h':
+        return duration_cast<hours>(ms).count();
+    default:
+        return -1;
+    }
 };
 
 string Timer::format_elapsed_time() {
-	string result;
-	stringstream str;
-	// Each number must be write in 2 digits
-	str << setw(2) << setfill('0') << get_elapsed_time('h') << ":";
-	str << setw(2) << setfill('0') << get_elapsed_time('m') << ":";
-	str << setw(2) << setfill('0') << get_elapsed_time('s') << endl;
-	str >> result;
-	return result;
+    string result;
+    stringstream str;
+    // Each number must be write in 2 digits
+    str << setw(2) << setfill('0') << get_elapsed_time('h') << ":";
+    str << setw(2) << setfill('0') << get_elapsed_time('m') << ":";
+    str << setw(2) << setfill('0') << get_elapsed_time('s') << endl;
+    str >> result;
+    return result;
 };
